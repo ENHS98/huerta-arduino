dc79666dc6e781f91a288dac9d3d63339798aa5f
ENHS98 <nicolas-e-hein@hotmail.com>
2025-04-23 20:40:05 -0300

    Agregar modelo y migración para la tabla 'valores'

---
+++ app/Models/Valor.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Valor extends Model
+{
+    protected $table = 'valores';
+
+    protected $fillable = [
+        'temperatura',
+        'humedad',
+        'uv',
+        'fecha',
+        'hora',
+    ];
+}
